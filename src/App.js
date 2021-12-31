47c1a413ac38307452115eb82215b3cc24efaa75
Rano Kuhl <ranokuhl@gmail.com>
2021-12-31 23:45:16 +0100

    add schema

--- src/App.js
+++ src/App.js
@@ -1,7 +1,40 @@
 import './App.css'
+import Form from 'react-jsonschema-form'
+
+const schema = {
+	title: 'A registration form',
+	description: 'A simple form example.',
+	type: 'object',
+	required: ['firstName', 'lastName'],
+	properties: {
+		firstName: {
+			type: 'string',
+			title: 'First name',
+			default: 'Chuck',
+		},
+		lastName: {
+			type: 'string',
+			title: 'Last name',
+		},
+		telephone: {
+			type: 'string',
+			title: 'Telephone',
+			minLength: 10,
+		},
+	},
+}
 
 function App() {
-	return <div className='App'></div>
+	return (
+		<div className='App'>
+			<Form
+				schema={schema}
+				onSubmit={({FormData}) =>
+					alert(JSON.stringify(FormData, null, 2))
+				}
+			></Form>
+		</div>
+	)
 }
 
 export default App
